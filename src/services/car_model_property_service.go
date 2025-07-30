f2fe4a70e2bd75d825a4caa43a421393b581a440
Hamidreza <hamidlashgari1998@gmail.com>
2025-07-30 17:21:33 +0330

    feat: implement car model dynamic property crud

---
+++ src/services/car_model_property_service.go
@@ -0,0 +1,51 @@
+package services
+
+import (
+	"context"
+	"github.com/hvmidrezv/web-app/api/dto"
+	"github.com/hvmidrezv/web-app/config"
+	"github.com/hvmidrezv/web-app/data/db"
+	"github.com/hvmidrezv/web-app/data/models"
+	"github.com/hvmidrezv/web-app/pkg/logging"
+)
+
+type CarModelPropertyService struct {
+	base *BaseService[models.CarModelProperty, dto.CreateCarModelPropertyRequest, dto.UpdateCarModelPropertyRequest, dto.CarModelPropertyResponse]
+}
+
+func NewCarModelPropertyService(cfg *config.Config) *CarModelPropertyService {
+	return &CarModelPropertyService{
+		base: &BaseService[models.CarModelProperty, dto.CreateCarModelPropertyRequest, dto.UpdateCarModelPropertyRequest, dto.CarModelPropertyResponse]{
+			Database: db.GetDb(),
+			Logger:   logging.NewLogger(cfg),
+			Preloads: []preload{
+				{string: "Property.Category"},
+			},
+		},
+	}
+}
+
+// Create
+func (s *CarModelPropertyService) Create(ctx context.Context, req *dto.CreateCarModelPropertyRequest) (*dto.CarModelPropertyResponse, error) {
+	return s.base.Create(ctx, req)
+}
+
+// Update
+func (s *CarModelPropertyService) Update(ctx context.Context, id int, req *dto.UpdateCarModelPropertyRequest) (*dto.CarModelPropertyResponse, error) {
+	return s.base.Update(ctx, id, req)
+}
+
+// Delete
+func (s *CarModelPropertyService) Delete(ctx context.Context, id int) error {
+	return s.base.Delete(ctx, id)
+}
+
+// Get By Id
+func (s *CarModelPropertyService) GetById(ctx context.Context, id int) (*dto.CarModelPropertyResponse, error) {
+	return s.base.GetById(ctx, id)
+}
+
+// Get By Filter
+func (s *CarModelPropertyService) GetByFilter(ctx context.Context, req *dto.PaginationInputWithFilter) (*dto.PagedList[dto.CarModelPropertyResponse], error) {
+	return s.base.GetByFilter(ctx, req)
+}
